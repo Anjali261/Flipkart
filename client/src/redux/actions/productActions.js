17093d3e6e05e20d5f85b87ffa610583d95d15b6
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-10-09 10:11:59 +0530

    Actions setup for Product

---
+++ client/src/redux/actions/productActions.js
@@ -0,0 +1,33 @@
+
+// using redux you have to take help of middleware.
+// 
+
+import axios from 'axios'
+import React from 'react'
+import * as actionTypes from '../constants/productConstant'
+const URL = "http://localhost:8000"; 
+export const getProducts = () => async (dispatch) => {
+ try{
+  const {data} =  await axios.get(`${URL}/products`);
+//   console.log(response);
+dispatch({type:actionTypes.GET_PRODUCT_SUCCESS, payload:data});
+ }catch(error){
+    dispatch({type:actionTypes.GET_PRODUCT_FAIL,paylaod:error.message})
+ }
+}
+
+export default getProducts
+
+export const getProductDetails =(id) => async(dispatch) =>{
+   try{
+      dispatch({type: actionTypes.GET_PRODUCT_DETAILS_REQUEST})
+      const {data} =  await axios.get(`${URL}/product/${id}`);
+      dispatch({type:actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload:data});
+
+   }catch(error){
+      dispatch({type:actionTypes.GET_PRODUCT_DETAILS_FAIL,paylaod:error.message})
+
+
+
+   }
+}
