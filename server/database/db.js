3e149db5a6bc8f2ec7c1f16454be485f0df25a0d
Anjali261 <mail2anjali.singh2610@gmail.com>
2023-10-09 09:34:17 +0530

    Database and Schema

---
+++ server/database/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = async() =>{
+    const URL=  `mongodb+srv://${USERNAME}:${PASSWORD}@flipkart.cmfnnm2.mongodb.net/`
+    // const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@socialmedia.3nl5wz4.mongodb.net/`
+    try{
+       await  mongoose.connect(URL, {
+        useNewUrlParser:true,
+    });
+        console.log('Database Connected Successfully');
+
+    }catch(error){
+        console.log('Error while Connecting with the database ', error.message);
+    }
+}
+
+export default Connection;
+
